b24a5abac65d378d601883aa4404f19eae05bf97
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-05 15:48:56 +0530

    Update clientwebappchange.py

--- clientwebappchange.py
+++ clientwebappchange.py
@@ -84,8 +84,8 @@ def main():
 
         # Create an empty shopping list
         shopping_list = st.text_input("Add items to your shopping list (comma-separated)")
-        #st.write(csv_data)
-        #st.write(te_ary)
+        st.write(csv_data)
+        st.write(te_ary)
         # Create a dropdown with items from the database
         item_list = te.columns_  # Assuming the columns are item names
         st.write(item_list)
