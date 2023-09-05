d5450ddfc3a580662b1d478de5c62b2d7954f3dd
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-05 16:47:45 +0530

    Update clientwebappchange.py

--- clientwebappchange.py
+++ clientwebappchange.py
@@ -26,7 +26,7 @@ te = TransactionEncoder()
 
 # Transform the data into a one-hot encoded DataFrame
 #te_ary = te.fit_transform(csv_data)
-#te_ary = te.fit(data_list).transform(data_list)
+te_ary = te.fit(data_list).transform(data_list)
 
 
 # Initialize min_support and min_confidence as global variables with default values
