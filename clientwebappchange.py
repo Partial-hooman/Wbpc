a3f735d7e7ccbf2aadb95d9495ab770633badbe4
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-10 06:15:59 +0530

    Update clientwebappchange.py

--- clientwebappchange.py
+++ clientwebappchange.py
@@ -152,8 +152,8 @@ def main():
             
            st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
            
-         st.write("items present in shopping list:")
-         st.dataframe(st.session_state.shopping_list)
+        st.write("items present in shopping list:")
+        st.dataframe(st.session_state.shopping_list)
          
     elif page == "Options":
         st.header("Options")
