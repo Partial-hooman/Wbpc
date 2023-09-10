a834c386fc959a515f77f33909afe2a4e8ecaf9f
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-10 16:49:33 +0530

    Update clientwebappchange.py

--- clientwebappchange.py
+++ clientwebappchange.py
@@ -159,7 +159,7 @@ def main():
                st.write("recommended_items:")
                rc = list(top_items.head(5)["Item"]) 
                #st.write(list(top_items.head(5)["Item"]))
-               st.write(e)
+               #st.write(e)
          #rc_itms = []
          #st.write(rc)
          #for itm in rc:
@@ -167,27 +167,27 @@ def main():
          if st.button(rc[0],key="0"):
                 if rc[0] not in st.session_state.shopping_list:
                  st.session_state.shopping_list.append(rc[0])
-                 st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
+                 #st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
                  st.experimental_rerun()
          if st.button(rc[1],key="1"):
                 if rc[1] not in st.session_state.shopping_list:
                  st.session_state.shopping_list.append(rc[1])
-                 st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
+                 #st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
                  st.experimental_rerun()
          if st.button(rc[2],key="2"):
                 if rc[2] not in st.session_state.shopping_list:
                  st.session_state.shopping_list.append(rc[2])
-                 st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
+                 #st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
                  st.experimental_rerun()
          if st.button(rc[3],key="3"):
                 if rc[3] not in st.session_state.shopping_list:
                  st.session_state.shopping_list.append(rc[3])
-                 st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
+                 #st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
                  st.experimental_rerun()
          if st.button(rc[4],key="4"):
                 if rc[4] not in st.session_state.shopping_list:
                  st.session_state.shopping_list.append(rc[4])
-                 st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
+                 #st.success("Items added to your shopping list: {}".format(st.session_state.shopping_list))
                  st.experimental_rerun()
               #rc_itms.append(itm)   
         #selected_rc_item = st.multiselect("Select an item from recommended items", rc)
